beae4bded9d6bb939174df6f9dcef6c6cbc83ba8
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2021-04-28 00:51:36 -0300

    finalizando

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,7 +6,7 @@ socket.on("admin_list_all_users", (connections) => {
     connectionsUsers = connections
 
     document.getElementById("list_users").innerHTML = ""
-    
+
     let template = document.getElementById("template").innerHTML
 
     connections.forEach(connection => {
@@ -37,18 +37,75 @@ function call(id) {
         user_id: connection.user_id
     }
 
+    socket.emit("admin_user_in_support", params)
+
     socket.emit("admin_list_messages_by_user", params, messages => {
         const divMessages = document.getElementById(`allMessages${connection.user_id}`)
 
         messages.forEach(message => {
             const createDiv = document.createElement("div")
 
-            if(message.admin_id === null){
+            if (message.admin_id === null) {
+                createDiv.className = "admin_message_client"
 
-            }else{
-                
+                createDiv.innerHTML = `<span>${connection.user.email} </span>`;
+                createDiv.innerHTML += `<span>${message.text}</span>`;
+
+                createDiv.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`
+
+            } else {
+                createDiv.className = "admin_message_admin"
+
+                createDiv.innerHTML = `Atendente: <span>${message.text}</span>`;
+                createDiv.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`
             }
+            divMessages.appendChild(createDiv)
         })
     })
-
 }
+
+function sendMessage(id) {
+    const text = document.getElementById(`send_message_${id}`)
+
+    const params = {
+        text: text.value,
+        user_id: id
+    }
+
+    socket.emit('admin_send_message', params)
+
+    const divMessages = document.getElementById(`allMessages${id}`)
+
+    console.log('DIVMESSAGES 1 ', divMessages)
+
+    const createDiv = document.createElement("div")
+    createDiv.className = "admin_message_admin"
+
+    createDiv.innerHTML = `Atendente: <span>${params.text} </span>`;
+
+    createDiv.innerHTML += `<span class="admin_date">${dayjs().format("DD/MM/YYYY HH:mm:ss")}`
+
+    divMessages.appendChild(createDiv)
+
+    text.value = ''
+}
+
+socket.on("admin_receive_message", data => {
+
+    
+    const connection = connectionsUsers.find(connection => connection.socket_id === data.socket_id)
+
+    const divMessages = document.getElementById(`allMessages${connection.user_id}`)
+
+    const createDiv = document.createElement("div")
+
+    createDiv.className = "admin_message_client"
+
+    createDiv.innerHTML = `<span>${connection.user.email} </span>`;
+    createDiv.innerHTML += `<span>${data.message.text}</span>`;
+
+    createDiv.innerHTML += `<span class="admin_date">${dayjs(data.message.created_at).format("DD/MM/YYYY HH:mm:ss")}`
+    
+    divMessages.appendChild(createDiv)
+    
+})
